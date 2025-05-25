bc1e5f0e621c81314a948fc0acc2952266b43898
Gzetta17 <gonzalooadanchu@gmail.com>
2025-05-24 21:24:58 -0300

    Diseños servicios individuales

--- js/animaciones.js
+++ js/animaciones.js
@@ -101,5 +101,18 @@ document.querySelectorAll('.gallery img').forEach(img => {
 //Fin galería Index
 
 
+//Tarjetas Información Telefonia//
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+  }
 
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
 
+  window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+      if (event.target === modal) modal.style.display = "none";
+    });
+  }
